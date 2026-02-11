f52efc922535eaf09e6cdbebc64dced427bc8389
Josie5734 <armyjoe999@gmail.com>
2026-02-11 01:00:06 +0000

    started on scores menu. created menu but no logic to fill it yet

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -10,6 +10,10 @@ func _on_options_pressed() -> void:
 	pass # Replace with function body.
 
 
+func _on_scores_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/scores_menu/scores_menu.tscn")
+
+
 # quit game
 func _on_exit_pressed() -> void:
 	get_tree().quit()
